35e77d44267313fae2b410550e384dcb59a1ff37
ryo <ba068082@gmail.com>
2017-12-07 14:58:44 +0900

    my first committe

---
+++ _posts/2017-12-3-testpost.md
@@ -0,0 +1,11 @@
+# testのポストなり
+
+# シンタックスハイライトのテスト
+
+ ```ruby
+class Test
+  def test
+    p test
+  end
+end
+ ```
---
+++ test.rb
@@ -0,0 +1,4 @@
+"test": "test",
+  test: "test"
+zsh:1: command not found: jq
+cat: test.rb: No such file or directory
